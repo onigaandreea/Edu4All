9342ed2d5e1b57854dea98a424a24bfe3d9bf3fa
Andreea <92400825+onigaandreea@users.noreply.github.com>
2023-05-05 15:18:42 +0300

    account repo

--- Edu4All/Persistence/src/main/java/project/AccountRepository.java
+++ Edu4All/Persistence/src/main/java/project/AccountRepository.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class AccountRepository implements IAccountRepo{
@@ -25,6 +27,7 @@ public class AccountRepository implements IAccountRepo{
         try(PreparedStatement preStmt=con.prepareStatement("insert into Account (accountNr, CVV, expDate) values (?, ?, ?)")){
             preStmt.setString(1, elem.getAccountNr());
             preStmt.setString(2, elem.getCVV());
+            preStmt.setString(3, elem.getExpDate());
             int result= preStmt.executeUpdate();
             logger.trace("Saved {} instances", elem);
         }catch (SQLException ex){
@@ -36,21 +39,86 @@ public class AccountRepository implements IAccountRepo{
 
     @Override
     public void delete(Account elem) {
-
+        logger.traceEntry("deleting task {} ", elem);
+        Connection con= dbUtils.getConnection();
+        try(PreparedStatement preStmt=con.prepareStatement("delete * from Account where idA = ?")){
+            preStmt.setInt(1, elem.getId());
+            int result= preStmt.executeUpdate();
+            logger.trace("Deleted {} instances", elem);
+        }catch (SQLException ex){
+            logger.error(ex);
+            System.err.println("Error DB " + ex);
+        }
+        logger.traceExit();
     }
 
     @Override
     public void update(Account elem, Integer id) {
-
+        logger.traceEntry("updating task {} ", elem);
+        Connection con= dbUtils.getConnection();
+        try(PreparedStatement preStmt=con.prepareStatement("update Account set accountNr = ?, CVV = ? , expDate = ? where idA = ?")){
+            preStmt.setString(1, elem.getAccountNr());
+            preStmt.setString(2, elem.getCVV());
+            preStmt.setString(3, elem.getExpDate());
+            preStmt.setInt(4, elem.getId());
+            int result= preStmt.executeUpdate();
+            logger.trace("Updated {} instances", elem);
+        }catch (SQLException ex){
+            logger.error(ex);
+            System.err.println("Error DB " + ex);
+        }
+        logger.traceExit();
     }
 
     @Override
     public Account findById(Integer id) {
+        logger.traceEntry();
+        Connection con= dbUtils.getConnection();
+        try (PreparedStatement preStmt=con.prepareStatement("select * from Account where idA = ?")){
+            preStmt.setInt(1, id);
+            try (ResultSet result= preStmt.executeQuery()){
+                while (result.next()){
+                    int idA = result.getInt("idA");
+                    String accountNr = result.getString("accountNr");
+                    String CVV = result.getString("CVV");
+                    String expDate = result.getString("expDate");
+
+                    Account account = new Account(accountNr, CVV, expDate);
+                    account.setId(idA);
+                    return account;
+                }
+            }
+        }catch (SQLException ex){
+            logger.error(ex);
+            System.err.println("Error DB " + ex);
+        }
+        logger.traceExit();
         return null;
     }
 
     @Override
     public Iterable<Account> findAll() {
-        return null;
+        logger.traceEntry();
+        Connection con= dbUtils.getConnection();
+        List<Account> accounts = new ArrayList<>();
+        try (PreparedStatement preStmt=con.prepareStatement("select * from Account")){
+            try (ResultSet result= preStmt.executeQuery()){
+                while (result.next()){
+                    int idA = result.getInt("idA");
+                    String accountNr = result.getString("accountNr");
+                    String CVV = result.getString("CVV");
+                    String expDate = result.getString("expDate");
+
+                    Account account = new Account(accountNr, CVV, expDate);
+                    account.setId(idA);
+                    accounts.add(account);
+                }
+            }
+        }catch (SQLException ex){
+            logger.error(ex);
+            System.err.println("Error DB " + ex);
+        }
+        logger.traceExit(accounts);
+        return accounts;
     }
 }
